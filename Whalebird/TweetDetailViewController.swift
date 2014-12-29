c117b684bb7b92df51b65f596d2ecd3c6a62754e
h3poteto <h3.poteto@gmail.com>
2014-12-29 23:08:58 +0900

    行間を再修正

--- Whalebird/TweetDetailViewController.swift
+++ Whalebird/TweetDetailViewController.swift
@@ -131,7 +131,7 @@ class TweetDetailViewController: UIViewController, UIActionSheetDelegate, UIText
         self.screenNameLabel.addTarget(self, action: "tappedUserProfile", forControlEvents: UIControlEvents.TouchDown)
         self.blankView.addSubview(self.screenNameLabel)
         
-        self.tweetBodyLabel = UITextView(frame: CGRectMake(cWindowSize.size.width * 0.05, self.profileImageLabel.frame.origin.y + self.profileImageLabel.frame.size.height + self.LabelPadding + 10, cWindowSize.size.width * 0.9, 15))
+        self.tweetBodyLabel = UITextView(frame: CGRectMake(cWindowSize.size.width * 0.05, self.profileImageLabel.frame.origin.y + self.profileImageLabel.frame.size.height + self.LabelPadding + 5, cWindowSize.size.width * 0.9, 15))
         self.tweetBodyLabel.font = UIFont(name: TimelineViewCell.NormalFont, size: 15)
         self.tweetBodyLabel.text = WhalebirdAPIClient.escapeString(self.tweetBody!) as NSString
         self.tweetBodyLabel.delegate = self
@@ -143,7 +143,7 @@ class TweetDetailViewController: UIViewController, UIActionSheetDelegate, UIText
         self.blankView.addSubview(self.tweetBodyLabel)
         
         
-        self.postDetailLabel = UILabel(frame: CGRectMake(cWindowSize.size.width * 0.05, self.tweetBodyLabel.frame.origin.y + self.tweetBodyLabel.frame.size.height + self.LabelPadding, cWindowSize.size.width * 0.9, 15))
+        self.postDetailLabel = UILabel(frame: CGRectMake(cWindowSize.size.width * 0.05, self.tweetBodyLabel.frame.origin.y + self.tweetBodyLabel.frame.size.height, cWindowSize.size.width * 0.9, 15))
         self.postDetailLabel.textAlignment = NSTextAlignment.Right
         self.postDetailLabel.text = self.postDetail
         self.postDetailLabel.font = UIFont(name: TimelineViewCell.NormalFont, size: 12)
@@ -238,7 +238,7 @@ class TweetDetailViewController: UIViewController, UIActionSheetDelegate, UIText
     
     // UITextViewの行間
     func layoutManager(layoutManager: NSLayoutManager, lineSpacingAfterGlyphAtIndex glyphIndex: Int, withProposedLineFragmentRect rect: CGRect) -> CGFloat {
-        return 4;
+        return 3;
     }
     
     func tappedReply() {
