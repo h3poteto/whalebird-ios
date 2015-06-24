b0df74f21fe37b002a557b9d24e433ee943476de
h3poteto <h3.poteto@gmail.com>
2015-06-25 00:48:55 +0900

    fixes #24 friend_idsではなくscreen_nameを受け取るように修正

--- Whalebird/FirendsList.swift
+++ Whalebird/FirendsList.swift
@@ -21,7 +21,7 @@ class FriendsList: NSObject {
     //====================================
     //  instance variables
     //====================================
-    var friendsList: Array<Int>?
+    var friendsList: Array<String>?
     var userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
     
     override init() {
@@ -37,23 +37,25 @@ class FriendsList: NSObject {
             let parameter: Dictionary<String, AnyObject> = [
                 "settings" : params
             ]
-            WhalebirdAPIClient.sharedClient.getDictionaryAPI("users/apis/friend_ids.json", params: parameter) { (aFollows) -> Void in
+            WhalebirdAPIClient.sharedClient.getArrayAPI("users/apis/friend_screen_names.json", params: parameter) { (aFollows) -> Void in
                 var q_main = dispatch_get_main_queue()
                 dispatch_async(q_main, {()->Void in
-                    if let ids = aFollows["ids"] as? Array<Int> {
-                        self.friendsList = ids
-                        self.userDefaults.setObject(self.friendsList, forKey: "friend_ids")
+                    if let screen_names = aFollows as? Array<AnyObject> {
+                        for name in screen_names {
+                            self.friendsList?.append((name.objectForKey("screen_name") as! String))
+                        }
+                        self.userDefaults.setObject(self.friendsList, forKey: "friend_screen_names")
                     }
                 })
             }
         }
     }
     
-    func getFriendsFromCache() -> Array<Int>? {
+    func getFriendsFromCache() -> Array<String>? {
         if let friends = self.friendsList {
             return friends
         } else {
-            if let friends = self.userDefaults.arrayForKey("friend_ids") as? Array<Int> {
+            if let friends = self.userDefaults.arrayForKey("friend_screen_names") as? Array<String> {
                 return friends
             }
             return nil
