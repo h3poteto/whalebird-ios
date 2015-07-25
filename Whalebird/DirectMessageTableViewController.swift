d3b656d2363b9367e73cdcbb75410af3651267df
h3poteto <h3.poteto@gmail.com>
2015-07-25 23:50:32 +0900

    refs #28 DirectMessageをTimelineModelに寄せる

--- Whalebird/DirectMessageTableViewController.swift
+++ Whalebird/DirectMessageTableViewController.swift
@@ -13,14 +13,9 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
     //=============================================
     //  instance variables
     //=============================================
-    var newMessage: Array<AnyObject> = []
-    var currentMessage: Array<AnyObject> = []
-    
     var refreshMessage: ODRefreshControl!
     var newMessageButton: UIBarButtonItem!
-    
-    var sinceId: String?
-    let tweetCount = Int(50)
+    var timelineModel: TimelineModel!
     
     //============================================
     //  instance methods
@@ -52,22 +47,10 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
         self.tableView.registerClass(TimelineViewCell.classForCoder(), forCellReuseIdentifier: "TimelineViewCell")
         
         var userDefaults = NSUserDefaults.standardUserDefaults()
-        var getSinceId = userDefaults.stringForKey("directMessageSinceId") as String?
-        self.sinceId = getSinceId
-        
+        var sinceId = userDefaults.stringForKey("directMessageSinceId") as String?
         var directMessage = userDefaults.arrayForKey("directMessage") as Array?
-        if (directMessage != nil) {
-            for message in directMessage! {
-                self.currentMessage.insert(message, atIndex: 0)
-            }
-            if var moreID = self.currentMessage.last?.objectForKey("id_str") as? String {
-                var readMoreDictionary = NSMutableDictionary(dictionary: [
-                    "moreID" : moreID,
-                    "sinceID" : "sinceID"
-                    ])
-                self.currentMessage.insert(readMoreDictionary, atIndex: self.currentMessage.count)
-            }
-        }
+        
+        self.timelineModel = TimelineModel(initSinceId: sinceId, initTimeline: directMessage)
 
     }
 
@@ -84,7 +67,7 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return self.currentMessage.count
+        return self.timelineModel.count()
     }
 
 
@@ -94,7 +77,7 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
             cell = TimelineViewCell(style: .Default, reuseIdentifier: "TimelineViewCell")
         }
         cell!.cleanCell()
-        if let targetMessage = self.currentMessage[indexPath.row] as? NSDictionary {
+        if let targetMessage = self.timelineModel.getTeetAtIndex(indexPath.row) {
             cell!.configureCell(targetMessage)
         }
 
@@ -104,7 +87,7 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
 
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         var height = CGFloat(60)
-        if let targetMessage = self.currentMessage[indexPath.row] as? NSDictionary {
+        if let targetMessage = self.timelineModel.getTeetAtIndex(indexPath.row) {
             height = TimelineViewCell.estimateCellHeight(targetMessage)
         }
         return height
@@ -113,15 +96,15 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
     
     override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         var height = CGFloat(60)
-        if let targetMessage = self.currentMessage[indexPath.row] as? NSDictionary {
+        if let targetMessage = self.timelineModel.getTeetAtIndex(indexPath.row) {
             height = TimelineViewCell.estimateCellHeight(targetMessage)
         }
         return height
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if let cMessageData = self.currentMessage[indexPath.row] as? NSDictionary {
-            if (cMessageData.objectForKey("moreID") != nil && cMessageData.objectForKey("moreID") as! String != "moreID") {
+        if let cMessageData = self.timelineModel.getTeetAtIndex(indexPath.row) {
+            if TimelineModel.selectMoreIdCell(cMessageData) {
                 var sinceID = cMessageData.objectForKey("sinceID") as? String
                 if (sinceID == "sinceID") {
                     sinceID = nil
@@ -143,113 +126,36 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
     }
     
     func updateMessage(aSinceID: String?, aMoreIndex: Int?) {
-        var params: Dictionary<String, String> = [
-            "count" : String(self.tweetCount)
-        ]
-        if (aSinceID != nil) {
-            params["since_id"] = aSinceID as String!
-        }
-        if (aMoreIndex != nil) {
-            if var strMoreID = (self.currentMessage[aMoreIndex!] as! NSDictionary).objectForKey("moreID") as? String {
-                // max_idは「以下」という判定になるので自身を含めない
-                params["max_id"] = BigInteger(string: strMoreID).decrement()
-            }
-        }
-        let cParameter: Dictionary<String, AnyObject> = [
-            "settings" : params
-        ]
         SVProgressHUD.showWithStatus("キャンセル", maskType: SVProgressHUDMaskType.Clear)
-        WhalebirdAPIClient.sharedClient.getArrayAPI("users/apis/direct_messages.json", displayError: true, params: cParameter,
-            completed: { [unowned self] (aNewMessage) -> Void in
-                var q_main = dispatch_get_main_queue()
-                dispatch_async(q_main, {()->Void in
-                    self.newMessage = aNewMessage as Array<AnyObject>
-                    var currentRowIndex: Int?
-                    if (self.newMessage.count > 0) {
-                        if (aMoreIndex == nil) {
-                            // refreshによる更新
-                            if (self.newMessage.count >= self.tweetCount) {
-                                var moreID = self.newMessage.first?.objectForKey("id_str") as! String
-                                var readMoreDictionary = NSMutableDictionary()
-                                if (self.currentMessage.count > 0) {
-                                    var sinceID = self.currentMessage.first?.objectForKey("id_str") as! String
-                                    readMoreDictionary = NSMutableDictionary(dictionary: [
-                                        "moreID" : moreID,
-                                        "sinceID" : sinceID
-                                        ])
-                                } else {
-                                    readMoreDictionary = NSMutableDictionary(dictionary: [
-                                        "moreID" : moreID,
-                                        "sinceID" : "sinceID"
-                                        ])
-                                }
-                                self.newMessage.insert(readMoreDictionary, atIndex: 0)
-                            }
-                            if (self.currentMessage.count > 0) {
-                                currentRowIndex = self.newMessage.count
-                            }
-                            for newTweet in self.newMessage {
-                                self.currentMessage.insert(newTweet, atIndex: 0)
-                                self.sinceId = (newTweet as! NSDictionary).objectForKey("id_str") as? String
-                            }
-                        } else {
-                            // readMoreを押した場合
-                            // tableの途中なのかbottomなのかの判定
-                            if (aMoreIndex == self.currentMessage.count - 1) {
-                                // bottom
-                                var moreID = self.newMessage.first?.objectForKey("id_str") as! String
-                                var readMoreDictionary = NSMutableDictionary(dictionary: [
-                                    "moreID" : moreID,
-                                    "sinceID" : "sinceID"
-                                    ])
-                                self.newMessage.insert(readMoreDictionary, atIndex: 0)
-                                self.currentMessage.removeLast()
-                                self.currentMessage += self.newMessage.reverse()
-                            } else {
-                                // 途中
-                                if (self.newMessage.count >= self.tweetCount) {
-                                    var moreID = self.newMessage.first?.objectForKey("id_str") as! String
-                                    var sinceID = (self.currentMessage[aMoreIndex! + 1] as! NSDictionary).objectForKey("id_str") as! String
-                                    var readMoreDictionary = NSMutableDictionary(dictionary: [
-                                        "moreID" : moreID,
-                                        "sinceID" : sinceID
-                                        ])
-                                    self.newMessage.insert(readMoreDictionary, atIndex: 0)
-                                }
-                                self.currentMessage.removeAtIndex(aMoreIndex!)
-                                for newTweet in self.newMessage {
-                                    self.currentMessage.insert(newTweet, atIndex: aMoreIndex!)
-                                }
-                                
-                            }
-                        }
-                        self.tableView.reloadData()
-                        var userDefault = NSUserDefaults.standardUserDefaults()
-                        if (currentRowIndex != nil && userDefault.integerForKey("afterUpdatePosition") == 2) {
-                            var indexPath = NSIndexPath(forRow: currentRowIndex!, inSection: 0)
-                            self.tableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Top, animated: false)
-                        }
-                        SVProgressHUD.dismiss()
-                        var notice = WBSuccessNoticeView.successNoticeInView(self.navigationController!.view, title: String(aNewMessage.count) + "件更新")
-                        notice.alpha = 0.8
-                        notice.originY = (UIApplication.sharedApplication().delegate as! AppDelegate).alertPosition
-                        notice.show()
-                    } else {
-                        SVProgressHUD.dismiss()
-                        var notice = WBSuccessNoticeView.successNoticeInView(self.navigationController!.view, title: "新着なし")
-                        notice.alpha = 0.8
-                        notice.originY = (UIApplication.sharedApplication().delegate as! AppDelegate).alertPosition
-                        notice.show()
-                    }
-                })
+        self.timelineModel.updateTimeline("users/apis/direct_messages.json", aSinceID: aSinceID, aMoreIndex: aMoreIndex,
+            completed: { (count, currentRowIndex) -> Void in
+                self.tableView.reloadData()
+                var userDefault = NSUserDefaults.standardUserDefaults()
+                if (currentRowIndex != nil && userDefault.integerForKey("afterUpdatePosition") == 2) {
+                    var indexPath = NSIndexPath(forRow: currentRowIndex!, inSection: 0)
+                    self.tableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Top, animated: false)
+                }
+                SVProgressHUD.dismiss()
+                var notice = WBSuccessNoticeView.successNoticeInView(self.navigationController!.view, title: String(count) + "件更新")
+                notice.alpha = 0.8
+                notice.originY = (UIApplication.sharedApplication().delegate as! AppDelegate).alertPosition
+                notice.show()
+                
+            }, noUpdated: { () -> Void in
+                SVProgressHUD.dismiss()
+                var notice = WBSuccessNoticeView.successNoticeInView(self.navigationController!.view, title: "新着なし")
+                notice.alpha = 0.8
+                notice.originY = (UIApplication.sharedApplication().delegate as! AppDelegate).alertPosition
+                notice.show()
             }, failed: { () -> Void in
+                
         })
     }
 
     
     func onRefresh() {
         self.refreshMessage.beginRefreshing()
-        updateMessage(self.sinceId, aMoreIndex: nil)
+        updateMessage(self.timelineModel.sinceId, aMoreIndex: nil)
         self.refreshMessage.endRefreshing()
         NotificationUnread.clearUnreadBadge()
     }
@@ -261,24 +167,11 @@ class DirectMessageTableViewController: UITableViewController, UITableViewDelega
     }
     
     func destroy() {
-        var userDefaults = NSUserDefaults.standardUserDefaults()
-        var cleanMessageArray: Array<NSMutableDictionary> = []
-        let cMessageMin = min(self.currentMessage.count, self.tweetCount)
-        if (cMessageMin < 1) {
-            return
-        }
-        for message in self.currentMessage[0...(cMessageMin - 1)] {
-            var dic = WhalebirdAPIClient.sharedClient.cleanDictionary(message as! NSDictionary)
-            cleanMessageArray.append(dic)
-        }
-        userDefaults.setObject(cleanMessageArray.reverse(), forKey: "directMessage")
-        userDefaults.setObject(self.sinceId, forKey: "directMessageSinceId")
+        self.timelineModel.saveCurrentTimeline("directMessage", sinceIdKey: "directMessageSinceId")
     }
     
     func clearData() {
-        self.currentMessage = []
-        self.newMessage = []
-        self.sinceId = nil
+        self.timelineModel.clearData()
         var userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setObject(nil, forKey: "directMessageSinceID")
         userDefaults.setObject(nil, forKey: "directMessage")
