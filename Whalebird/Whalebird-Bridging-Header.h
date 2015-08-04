3c3d74fc82f485f0019704f0458261a319bf5881
h3poteto <h3.poteto@gmail.com>
2015-08-05 02:38:09 +0900

    closes #34 Objective-Cのファイルを削除

--- Whalebird/Whalebird-Bridging-Header.h
+++ Whalebird/Whalebird-Bridging-Header.h
@@ -1,23 +1,4 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-/*
-#import <TSMessages/TSMessage.h>
-#import <NoticeView/WBNoticeView.h>
-#import <NoticeView/WBErrorNoticeView.h>
-#import <NoticeView/WBSuccessNoticeView.h>
-#import <NoticeView/WBStickyNoticeView.h>
-#import <OHAttributedLabel/OHAttributedLabel.h>
-#import <SVPullToRefresh/SVPullToRefresh.h>
-#import <AFNetworking/AFNetworking.h>
-#import <SVProgressHUD/SVProgressHUD.h>
-#import <TTTAttributedLabel/TTTAttributedLabel.h>
-#import <DACircularProgress/DACircularProgressView.h>
-#import <ODRefreshControl/ODRefreshControl.h>
-#import <SwipeView/SwipeView.h>
-#import <UrlShortener/UrlShortener.h>
-#import "OriginalCryp.h"
-#import <SDWebImage/UIImageView+WebCache.h>
-#import <SDWebImage/UIButton+WebCache.h>
- */
 #import "application.h"
