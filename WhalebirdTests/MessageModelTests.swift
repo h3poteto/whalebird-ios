87b2dcde76d8d5c60f967c36d35769cf1af72f94
h3poteto <h3.poteto@gmail.com>
2015-09-22 00:50:58 +0900

    closes #48 現状内容が書けないテストも一応ファイル追加

---
+++ WhalebirdTests/MessageModelTests.swift
@@ -0,0 +1,24 @@
+//
+//  MessageModelTests.swift
+//  Whalebird
+//
+//  Created by akirafukushima on 2015/09/22.
+//  Copyright © 2015年 AkiraFukushima. All rights reserved.
+//
+
+import XCTest
+@testable import Whalebird
+
+class MessageModelTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+}
